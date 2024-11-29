c2ebfc9ba951cec0736e94323de85705d82e164b
seyeon22 <sywolleych@gmail.com>
2024-11-29 20:30:50 +0900

    FEAT: 회원가입 기능 생성

--- server/src/main/java/com/springboot/harubi/Controller/MemberController.java
+++ server/src/main/java/com/springboot/harubi/Controller/MemberController.java
@@ -1,16 +1,22 @@
 package com.springboot.harubi.Controller;
 
+import com.springboot.harubi.Common.BaseResponse;
+import com.springboot.harubi.Domain.Dto.request.AuthRequestDto;
+import com.springboot.harubi.Service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
-    // 예시 코드 - 회원가입
-    //    @PostMapping("/auth/signup")
-    //    public BaseResponse signup(@RequestBody AuthRequestDto authRequestDto) {
-    //        memberService.join(authRequestDto);
-    //        return new BaseResponse(HttpStatus.OK.value(), "회원가입이 완료되었습니다.");
-    //    }
+    @PostMapping("/auth/signUp")
+    public BaseResponse signUp(@RequestBody AuthRequestDto authRequestDto) {
+        memberService.join(authRequestDto);
+        return new BaseResponse(HttpStatus.OK.value(), "회원가입이 완료되었습니다.");
+    }
 }
--- server/src/main/java/com/springboot/harubi/Service/MemberService.java
+++ server/src/main/java/com/springboot/harubi/Service/MemberService.java
@@ -1,5 +1,8 @@
 package com.springboot.harubi.Service;
 
+import com.springboot.harubi.Domain.Dto.request.AuthRequestDto;
+import com.springboot.harubi.Domain.Entity.Member;
+import com.springboot.harubi.Exception.BaseException;
 import com.springboot.harubi.Repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,11 +14,42 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    // 예시 코드 - 회원가입
-    //    @Transactional
-    //    public void join(AuthRequestDto authRequestDto) {
-    //        validateAuthReqeust(authRequestDto);
-    //        validateDuplicateEmail(authRequestDto.getEmail());
-    //        memberRepository.save(convertToMember(authRequestDto));
-    //    }
+    @Transactional
+    public void join(AuthRequestDto authRequestDto) {
+        validateAuthReqeust(authRequestDto);
+        validateDuplicateEmail(authRequestDto.getEmail());
+        memberRepository.save(convertToMember(authRequestDto));
+    }
+
+    private void validateAuthReqeust(AuthRequestDto authRequestDto) {
+        if (authRequestDto.getName() == null) {
+            throw new BaseException(400, "이름을 입력해 주세요.");
+        }
+        if (authRequestDto.getSign_id() == null) {
+            throw new BaseException(400, "아이디를 입력해 주세요.");
+        }
+        if (authRequestDto.getPassword() == null) {
+            throw new BaseException(400, "비밀번호를 입력해 주세요.");
+        }
+        if (authRequestDto.getNickname() == null) {
+            throw new BaseException(400, "닉네임을 입력해 주세요.");
+        }
+        if (authRequestDto.getEmail() == null) {
+            throw new BaseException(400, "이메일을 입력해 주세요.");
+        }
+        if (!authRequestDto.isAgreed()) {
+            throw new BaseException(400, "개인정보 수집에 동의해 주세요.");
+        }
+    }
+
+    // 중복된 이메일 체크
+    private void validateDuplicateEmail(String email) {
+        if (!memberRepository.findByEmail(email).stream().toList().isEmpty()) {
+            throw new BaseException(400, "이미 회원가입된 이메일");
+        }
+    }
+
+    private Member convertToMember(AuthRequestDto authRequestDto) {
+        return new Member(authRequestDto);
+    }
 }
